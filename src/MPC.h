202a3220f0db024c2948613f926d7ba5b0ab6b43
Pantelis Monogioudis <monogioudis@yahoo.com>
2017-09-04 13:07:09 -0400

    initial commit

--- src/MPC.h
+++ src/MPC.h
@@ -7,14 +7,20 @@
 using namespace std;
 
 class MPC {
- public:
-  MPC();
+public:
+    MPC();
 
-  virtual ~MPC();
+    virtual ~MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+    // Number of state variables
+    size_t Ns;
+    // Number of actuators
+    size_t Na;
+
 };
 
 #endif /* MPC_H */
